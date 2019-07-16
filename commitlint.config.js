bc234a3f11c14b63d78c27f622006ab671d1d912
fisker <lionkay@gmail.com>
2019-07-16 11:29:35 +0800

    chore: update configs

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,10 +1,10 @@
 /*!
  * config file for `commitlint`
  *
- * update: wget -O commitlint.config.js https://git.io/fhAJV
+ * update: wget -O commitlint.config.js https://git.io/fjVj4
  * document: https://git.io/fhAJa
  */
 
 module.exports = {
-  extends: ['@fisker/commitlint-config'],
+  extends: ['@fisker'],
 };
---
+++ prettier.config.js
@@ -0,0 +1,7 @@
+/*!
+ * config file for `prettier`
+ *
+ * document: https://prettier.io/docs/en/options.html
+ */
+
+module.exports = require('@xwtec/prettier-config');
